6de6a972354d40fbd6a18bc796ee90e07aa3ce3a
Mario <mario.luquero@educa.madrid.org>
2025-12-11 10:02:14 +0100

    Mi primer commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var a int
+
+	fmt.Println("INPUT BITCH: ")
+	fmt.Scanf("%v", &a)
+
+	switch a {
+	case 1:
+		fmt.Println(a)
+	case 2:
+		fmt.Println(a)
+	default:
+		fmt.Println("Hello World")
+	}
+	for i := a; i <= 10; i++ {
+		println(i)
+	}
+
+	var b string
+
+	fmt.Scanf("%v", &b)
+
+	var c bool = b == "a"
+
+	if c {
+		fmt.Println("Es igual")
+	} else {
+		fmt.Println("No es igual")
+	}
+}
